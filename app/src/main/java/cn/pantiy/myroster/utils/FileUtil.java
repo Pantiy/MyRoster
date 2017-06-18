1e103949b4d7137e3f85840e4cec97acd1abb9d2
Pantiy <pantiy@126.com>
2017-06-18 18:59:05 +0800

    FileUtil

---
+++ app/src/main/java/cn/pantiy/myroster/utils/FileUtil.java
@@ -0,0 +1,11 @@
+package cn.pantiy.myroster.utils;
+
+/**
+ * MyRoster
+ * cn.pantiy.myroster.utils
+ * Created by pantiy on 17-6-18.
+ * Copyright © 2017 All rights Reserved by Pantiy
+ */
+
+public class FileUtil {
+}
